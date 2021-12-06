bd2e35bdc2f256fdf5d51f6d890f88469b8f58a1
laarkell <laarkell@davidson.edu>
2021-12-05 20:02:22 -0500

    000 set view

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var map = L.map('map').setView([0, 0], 2);
+var map = L.map('map').setView([0, 0], 0);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
